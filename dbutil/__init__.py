c4683d42746b3cfbe56eabaec18f03a7afef8e5c
Tom Lee <github@tomlee.co>
2012-01-11 22:18:25 -0800

    Initial import: initial implementation of row, connection and connect().

---
+++ dbutil/__init__.py
@@ -0,0 +1,111 @@
+from urlparse import urlparse
+from pprint import saferepr
+
+version = (0, 1, 0)
+
+class row(object):
+    def __init__(self, desc, data):
+        object.__init__(self)
+        self.__data = dict(zip([x[0] for x in desc], data))
+
+    def __getitem__(self, key):
+        return self.__data[key]
+
+    def __getattr__(self, key):
+        try:
+            return self.__data[key]
+        except KeyError:
+            raise AttributeError
+
+    def __repr__(self):
+        return "row(%s)" % saferepr(self.__data)
+
+class connection(object):
+    def __init__(self, impl):
+        object.__init__(self)
+        self.impl = impl
+
+    def __getattr__(self, key):
+        return getattr(self.impl, key)
+
+    def getone(self, query, params=None):
+        if params is None:
+            params = tuple()
+        with self.impl as crs:
+            crs.execute(query, params)
+            if crs.rowcount == 0:
+                return None
+            else:
+                return crs.fetchone()[0]
+
+    def getrow(self, query, params=None):
+        if params is None:
+            params = tuple()
+        with self.impl as crs:
+            crs.execute(query, params)
+            if crs.rowcount == 0:
+                return None
+            else:
+                return row(crs.description, crs.fetchone())
+
+    def getall(self, query, params=None):
+        if params is None:
+            params = tuple()
+        with self.impl as crs:
+            crs.execute(query, params)
+            result = []
+            for rec in crs:
+                result.append(row(crs.description, rec))
+            return result
+
+    def execute(self, query, params=None):
+        if params is None:
+            params = tuple()
+        with self.impl as crs:
+            crs.execute(query, params)
+
+    def each(self, cb, query, params=None):
+        if params is None:
+            params = tuple()
+        with self.impl as crs:
+            crs.execute(query, params)
+            for rec in crs:
+                cb(row(crs.description, rec))
+
+    def map(self, cb, query, params=None):
+        if params is None:
+            params = tuple()
+        with self.impl as crs:
+            crs.execute(query, params)
+            result = []
+            for rec in crs:
+                result.append(cb(row(crs.description, rec)))
+            return result
+
+def _connect_mysql(url):
+    import MySQLdb as db
+    params = {}
+    if url.username:
+        params["user"] = url.username
+    else:
+        params["user"] = "root"
+    if url.password:
+        params["passwd"] = url.password
+    if url.hostname:
+        params["host"] = url.hostname
+    if url.port:
+        params["port"] = url.port
+    if url.path:
+        params["db"] = url.path[1:]
+    return connection(db.connect(**params))
+
+DEFAULT_LOOKUP = {
+    "mysql": _connect_mysql
+}
+
+def connect(url, lookup=None):
+    url = urlparse(url)
+    if lookup is None:
+        lookup = DEFAULT_LOOKUP
+    return lookup[url.scheme](url)
+
---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, find_packages
+
+import dbutil
+
+LONG_DESCRIPTION = """
+"""
+
+setup(name="dbutil",
+      version="%d.%d.%d" % dbutil.version,
+      description="Minimalist utility functions for the Python DB API",
+      long_description=LONG_DESCRIPTION,
+      author="Tom Lee",
+      author_email="pypi@tomlee.co",
+      maintainer="Tom Lee",
+      maintainer_email="pypi@tomlee.co",
+      url="http://github.com/thomaslee/python-mindbx",
+      license="GPLv3",
+      packages=find_packages(),
+)
