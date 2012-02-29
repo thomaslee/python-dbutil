16362859aae74f11d6c582dea1735f6a16655058
Tom Lee <github@tomlee.co>
2012-02-28 23:10:45 -0800

    Handle integer indices

--- dbutil/row.py
+++ dbutil/row.py
@@ -3,10 +3,17 @@ from pprint import saferepr
 class row(object):
     def __init__(self, desc, data):
         object.__init__(self)
+        self.__rawdata = data
         self.__data = dict(zip([x[0] for x in desc], data))
 
     def __getitem__(self, key):
-        return self.__data[key]
+        try:
+            return self.__data[key]
+        except KeyError:
+            try:
+                return self.__rawdata[key]
+            except KeyError:
+                raise KeyError, key
 
     def __getattr__(self, key):
         try:
